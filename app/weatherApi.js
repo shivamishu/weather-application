68c41b7af03a3410f4530aa63cea5c81b3ad20ca
SHRIVASTAV <shivam.shrivastav@sap.com>
2020-09-06 17:48:25 +0530

    api route

---
+++ app/weatherApi.js
@@ -0,0 +1,34 @@
+export default class OpenWeatherApi {
+    constructor() {
+        this.baseUrl = 'http://api.openweathermap.org/data/2.5/'
+        this.apiKey = 'API_KEY'
+        this.api = 'weather'
+        this.query = 'q='
+        this.apiUrl = `${this.baseUrl}${this.api}?${this.query}&APPID=${this.apiKey}&units=metric`;
+    }
+
+    rebuildApiUrl() {
+        this.apiUrl = `${this.baseUrl}${this.api}?${this.query}&APPID=${this.apiKey}&units=metric`;
+    }
+
+    weatherFor(cityName) {
+        this.query = 'q=' + cityName;
+        this.api = 'weather'
+        this.rebuildApiUrl();
+        return this.apiUrl;
+    }
+
+    foreCastFor(cityName) {
+        this.query = 'q=' + cityName;
+        this.api = "forecast"
+        this.rebuildApiUrl();
+        return this.apiUrl;
+    }
+
+    weatherForByGeoLocation(lat, lon) {
+        this.query = `lat=${lat}&lon=${lon}`;
+        this.api = 'weather';
+        this.rebuildApiUrl();
+        return this.apiUrl;
+    }
+}
